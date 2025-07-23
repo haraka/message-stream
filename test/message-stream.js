6d4455c8d0b85b43d1df4bb7e5675e8d6100f443
Matt Simerson <matt@tnpi.net>
2025-07-23 14:51:28 -0700

    add tests

--- test/message-stream.js
+++ test/message-stream.js
@@ -25,7 +25,7 @@ describe('message-stream', () => {
   })
 })
 
-function getOutputFromStream(inputLines) {
+function getOutputFromStream(inputLines, pipeOpts) {
   return new Promise((resolve) => {
     const ms = new MessageStream({ main: {} }, 'msg', [])
     const output = new stream.PassThrough()
@@ -34,21 +34,47 @@ function getOutputFromStream(inputLines) {
     output.on('data', (chunk) => chunks.push(chunk.toString()))
     output.on('end', () => resolve(chunks.join('')))
 
-    ms.pipe(output, { dot_stuffed: true })
+    ms.pipe(output, pipeOpts)
 
     inputLines.forEach((line) => ms.add_line(line))
     ms.add_line_end()
   })
 }
 
-describe('dot-unstuffing', function () {
+describe('dot_stuffed = false', function () {
+  const pipeOpts = { dot_stuffed: false }
+
+  it('does not stuff "..\\r\\n', async () => {
+    const result = await getOutputFromStream(['..\r\n'], pipeOpts)
+    assert.match(result, /^..\r\n/m)
+  })
+
+  it('does not stuff "..dot start\\r\\n"', async () => {
+    const result = await getOutputFromStream(['..dot start\r\n'], pipeOpts)
+    assert.match(result, /^..dot start\r\n/m)
+  })
+
+  it('leaves normal lines untouched', async () => {
+    const result = await getOutputFromStream([
+      'hello\r\n',
+      '..dot line\r\n',
+      '..\r\n',
+    ], pipeOpts)
+
+    assert.equal(result, 'hello\r\n..dot line\r\n..\r\n')
+  })
+})
+
+describe('dot_stuffed = true', function () {
+  const pipeOpts = { dot_stuffed: true }
+
   it('unstuffs "..\\r\\n" to ".\\r\\n"', async () => {
-    const result = await getOutputFromStream(['..\r\n'])
+    const result = await getOutputFromStream(['..\r\n'], pipeOpts)
     assert.match(result, /^.\r\n/m)
   })
 
   it('unstuffs "..dot start\\r\\n" to ".dot start\\r\\n"', async () => {
-    const result = await getOutputFromStream(['..dot start\r\n'])
+    const result = await getOutputFromStream(['..dot start\r\n'], pipeOpts)
     assert.match(result, /^.dot start\r\n/m)
   })
 
@@ -57,11 +83,36 @@ describe('dot-unstuffing', function () {
       'hello\r\n',
       '..dot line\r\n',
       '..\r\n',
-    ])
+    ], pipeOpts)
 
     assert.equal(result, 'hello\r\n.dot line\r\n.\r\n')
     assert.match(result, /^hello\r\n/m)
     assert.match(result, /^.dot line\r\n/m)
     assert.match(result, /^.\r\n/m)
+  })
+})
+
+describe('dot_stuffing = false (legacy)', function () {
+  // sunset, delete after 2026
+  const pipeOpts = { dot_stuffing: false }
+
+  it('unstuffs "..\\r\\n" to ".\\r\\n"', async () => {
+    const result = await getOutputFromStream(['..\r\n'], pipeOpts)
+    assert.match(result, /^.\r\n/m)
+  })
+
+  it('unstuffs "..dot start\\r\\n" to ".dot start\\r\\n"', async () => {
+    const result = await getOutputFromStream(['..dot start\r\n'], pipeOpts)
+    assert.match(result, /^.dot start\r\n/m)
+  })
+
+  it('leaves normal lines untouched', async () => {
+    const result = await getOutputFromStream([
+      'hello\r\n',
+      '..dot line\r\n',
+      '..\r\n',
+    ], pipeOpts)
+
+    assert.equal(result, 'hello\r\n.dot line\r\n.\r\n')
   })
 })
